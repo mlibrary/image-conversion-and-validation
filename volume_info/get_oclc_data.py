c32cd3c319f3b73cc7b73881226e05a2aa599b88
Matthew Alexander LaChance <mattlach@umich.edu>
2016-12-06 11:05:03 -0500

    Write ArgumentCollector to pass tests

--- volume_info/get_oclc_data.py
+++ volume_info/get_oclc_data.py
@@ -506,3 +506,250 @@ class TabularData (Sequence):
             # the header row. Therefore, we can just pass it along. The
             # last row is still the last row.
             return self.__rows[key]
+
+class ArgumentCollector (Mapping):
+    """Mapping that with unchanging default values that can only be
+    modified with single `update` calls."""
+
+    def __init__ (self, *args, **kwargs):
+        """Initialize the argument collector with required and default
+        values."""
+
+        # Be sure we have the genuine arguments.
+        args, kwargs = self.__get_args_and_kwargs(args, kwargs)
+
+        # The positional arguments are the names of the arguments our
+        # update method will expect.
+        self.__expected_args = args
+
+        # The keyword arguments represent our default values.
+        self.__optional_args = kwargs
+
+        # Otherwise, we start with no given arguments.
+        self.__current_args = { }
+
+    def update (self, *args, **kwargs):
+        """Set a particular set of values.
+
+        The expected positional arguments will match up with the
+        positional arguments that you gave to init the object. Other
+        than that, you can give whatever keyword arguments you want.
+
+        Raises TypeError if you give weird arguments.
+        """
+
+        # First, make sure we can parse the new arguments without any
+        # problems.
+        mapping = self.__parse_new_args("update", args, kwargs)
+
+        # We have a valid mapping, so we'll override whatever we used to
+        # have.
+        self.__current_args = mapping
+
+    def __getitem__ (self, key):
+        """Return the value for the given key."""
+
+        if key in self.__current_args:
+            # Check the up-to-date given arguments first.
+            return self.__current_args[key]
+
+        else:
+            # If it's not in there, try to use a default. If it's not in
+            # there, we'll just pass along its KeyError.
+            return self.__optional_args[key]
+
+    def __iter__ (self):
+        """Iterate through present keys."""
+
+        # First, iterate through any defaults that aren't overridden.
+        # Second, iterate through the current specified set.
+        yield from self.__defaults_minus_current()
+        yield from self.__current_args
+
+    def __len__ (self):
+        """Return the count of key-value pairs currently inside."""
+
+        # Our length can start as the sum of our defaults and current
+        # keypair lengths.
+        result = len(self.__optional_args) + len(self.__current_args)
+
+        for key in self.__current_args:
+            if key in self.__optional_args:
+                # If a key is in both, then we've counted it twice, so
+                # we'll decrement our result to compensate.
+                result -= 1
+
+        return result
+
+    ################################################################
+    ################ Private Properties and Methods ################
+    ################################################################
+
+    def __defaults_minus_current (self):
+        # Loop through every key that isn't present in our current set
+        # of values.
+        for key in self.__optional_args:
+            # Check every optional key.
+            if key not in self.__current_args:
+                # Only yield it if it hasn't been overridden by an
+                # update().
+                yield key
+
+    def __get_args_and_kwargs (self, args, kwargs):
+        # I want the user to have the ability to give args and kwargs as
+        # premade arguments (rather than require variadic syntax).
+        if len(args) == 2                           \
+                and len(kwargs) == 0                \
+                and isinstance(args[0], Sequence)   \
+                and isinstance(args[1], Mapping):
+            # If the user gave exactly two arguments, and those
+            # arguments are a sequence and a mapping, then they must
+            # have specified their args and kwargs.
+            return args
+
+        else:
+            # Otherwise, assume they used the variadic syntax and return
+            # these two as they are.
+            return args, kwargs
+
+    def __parse_new_args (self, function_name, args, kwargs):
+        # We'll be returning a dict, and we start with a fresh one.
+        mapping = { }
+
+        # Be sure we're interpreting the arguments correctly and that
+        # there aren't too many.
+        args, kwargs = self.__validate_args_for_update(
+                function_name, args, kwargs)
+
+        self.__fill_in_all_args(function_name, args, kwargs, mapping)
+
+        # Raise an exception if we're missing any required arguments.
+        self.__raise_if_missing_args(function_name, mapping)
+
+        return mapping
+
+    def __validate_args_for_update (self, function_name, args, kwargs):
+        # Be sure we have the genuine arguments.
+        args, kwargs = self.__get_args_and_kwargs(args, kwargs)
+
+        if len(args) > len(self.__expected_args):
+            # If the positional argument list is longer than our list of
+            # expected arguments, then we have too many arguments.
+            self.__raise_too_many_arguments(function_name, len(args))
+
+        return args, kwargs
+
+    def __fill_in_all_args (self, function_name, args, kwargs, mapping):
+        # Pull out any positional arguments.
+        self.__fill_in_positional_args(
+                function_name, args, kwargs, mapping)
+
+        # Fill in the remaining (keyword) arguments.
+        mapping.update(kwargs)
+
+    def __fill_in_positional_args (self,
+            function_name, args, kwargs, mapping):
+        # We'll loop through the expected argument keys in step with our
+        # positional arguments to get key-value pairs.
+        for key, value in zip(self.__expected_args, args):
+            # Raise an error if this key is used positionally *and* as
+            # an explicit keyword.
+            self.__raise_if_duplicate_key(function_name, key, kwargs)
+
+            # If we're ok, add the keypair.
+            mapping[key] = value
+
+    def __raise_if_duplicate_key (self, function_name, key, kwargs):
+        if key in kwargs:
+            # We received this key as a positional argument, and now a
+            # second time as a keyword argument. That means we've
+            # received multiple values with one key.
+            self.__raise_multiple_values_one_key(function_name, key)
+
+    def __raise_if_missing_args (self, function_name, mapping):
+        # Get a list of all missing keys.
+        missing = self.__find_any_missing_args(mapping)
+
+        if missing:
+            # If we found any, raise an error.
+            self.__raise_missing_args(function_name, missing)
+
+    def __find_any_missing_args (self, mapping, missing = [ ]):
+        for key in self.__expected_args:
+            # Look at each key we expect to see.
+            self.__add_key_if_missing(key, mapping, missing)
+
+        return missing
+
+    def __add_key_if_missing (self, key, mapping, missing):
+        if key not in mapping:
+            # If this key isn't in our mapping, it's missing.
+            missing.append(key)
+
+    def __raise_missing_args (self, function_name, missing):
+        # Raise the error message using an english list of reprs of the
+        # missing keys.
+        self.__raise_missing_positional_args(
+                function_name,
+                len(missing),
+                self.__english_repr_list(missing))
+
+    def __english_repr_list (self, elts):
+        if len(elts) > 2:
+            # If we have more than two items, then we want everything
+            # separated by commas with a trailing "and" before the last
+            # item.
+            return "{}, and {}".format(
+                    ", ".join(map(repr, elts[:-1])),
+                    repr(elts[-1]))
+
+        else:
+            # If we have two items or fewer, we don't need commas. Since
+            # there are only three possible cases, I'll just list them:
+            #
+            #     []        => ""
+            #     [a]       => "repr(a)"
+            #     [a, b]    => "repr(a) and repr(b)"
+            return " and ".join(map(repr, elts))
+
+    def __maybe_plural (self, n, word, plural = None):
+        if n == 1:
+            # If there's just 1, then we don't need to care about the
+            # plural at all.
+            return "1 " + word
+
+        else:
+            # Otherwise, we may need to add an s to the word, depending
+            # on whether we were actually given a plural alternative.
+            return "{:d} {}".format(n, self.__add_s(word, plural))
+
+    def __add_s (self, word, plural):
+        if plural is None:
+            # By default, the plural just adds an s.
+            return word + "s"
+
+        else:
+            # Otherwise, we default to whatever the user says.
+            return plural
+
+    def __raise_too_many_arguments (self, function_name, args_length):
+        raise TypeError("{}() takes {} but {} given".format(
+                function_name,
+                self.__maybe_plural(
+                        len(self.__expected_args),
+                        "positional argument"),
+                self.__maybe_plural(args_length, "was", "were")))
+
+    def __raise_multiple_values_one_key (self, function_name, key):
+        raise TypeError(
+                "{}() got multiple values for argument {}".format(
+                        function_name,
+                        repr(key)))
+
+    def __raise_missing_positional_args (self,
+            function_name, count, arg_str):
+        raise TypeError("{}() missing {}: {}".format(
+                function_name,
+                self.__maybe_plural(count,
+                        "required positional argument"),
+                arg_str))
