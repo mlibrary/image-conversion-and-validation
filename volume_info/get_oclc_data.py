63a52ffb7f78e74fb3828b1f706d70844ea48ec3
Matthew Alexander LaChance <mattlach@umich.edu>
2016-12-08 10:58:28 -0500

    Write comments

--- volume_info/get_oclc_data.py
+++ volume_info/get_oclc_data.py
@@ -834,8 +834,11 @@ class ArgumentCollector (Mapping):
                 arg_str))
 
 class URI:
+    """URI object that handles GET and/or POST data."""
 
     def __init__ (self, *args, **kwargs):
+        """Init URI with base, expected args, and default data."""
+
         # The base URI should be the first positional argument.
         self.__uri = self.__extract_uri(args)
 
@@ -843,34 +846,62 @@ class URI:
         self.__args = ArgumentCollector(args[1:], kwargs)
 
     def get (self, *args, **kwargs):
+        """Return a URI bytes object with appropriate GET data."""
+
+        # Get the data.
         data = self.__get_encoded_data(args, kwargs)
 
         if data:
+            # If we have any data, it's appended to the URI with a
+            # question mark.
             return self.__uri + b"?" + data
 
         else:
+            # If we have no data, we can just use the URI as-is.
             return self.__uri
 
     def post (self, *args, **kwargs):
+        """Return a duple of bytes objects: base URI and POST data."""
         return self.__uri, self.__get_encoded_data(args, kwargs)
 
+    ################################################################
+    ################ Private Properties and Methods ################
+    ################################################################
+
+    def __extract_uri (self, positional_args):
+        if positional_args:
+            # If we have any arguments, then we're looking at the first
+            # one. It needs to be ASCII bytes.
+            return self.__assert_bytes(positional_args[0])
+
+        else:
+            # If we don't have any positional arguments, it's time to
+            # raise a TypeError telling the user about it.
+            #
+            # Normally, it'd be better to explicitly include a named
+            # argument "uri_base" in the function, but I don't want to
+            # limit the names of keyword arguments. If I put that there,
+            # and one of the keyword arguments was keyed on uri_base,
+            # then it'd override that instead of being passed on to the
+            # argument collector.
+            raise TypeError("__init__() missing 1 required " \
+                    "positional argument: 'uri_base'")
+
     def __get_encoded_data (self, args, kwargs):
+        # Update our internal argument collector.
         self.__args.update(args, kwargs)
+
+        # Run urllib.parse.urlencode on the mapping.
         encoded = urlencode(self.__args)
 
+        # We might get a str, and we want bytes.
         return self.__assert_bytes(encoded)
 
     def __assert_bytes (self, ascii_str):
         if isinstance(ascii_str, bytes):
+            # If it's already bytes, we'll just assume it's all ASCII.
             return ascii_str
 
         else:
+            # If it's a str, we'll need to encode it into bytes.
             return ascii_str.encode("ascii")
-
-    def __extract_uri (self, args):
-        if args:
-            return self.__assert_bytes(args[0])
-
-        else:
-            raise TypeError("__init__() missing 1 required " \
-                    "positional argument: 'uri_base'")
