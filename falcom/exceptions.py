4c49e938ddfa7fce3fe8e83e8b2cded68a9eb40c
Matthew Alexander LaChance <mattlach@umich.edu>
2016-12-20 12:49:51 -0500

    Move CountedWord into its own file

--- falcom/exceptions.py
+++ falcom/exceptions.py
@@ -3,6 +3,8 @@
 # BSD License. See LICENSE.txt for details.
 from re import compile as re_compile
 
+from .general.counted_word import CountedWord
+
 class BaseError (Exception):
     """Catchall exception to sire all others in this script."""
 
@@ -87,59 +89,6 @@ class InconsistentNewlines (InputFileError):
 class InconsistentColumnCounts (InputFileError):
     """Expected each row to have the same column count in {}"""
     pass
-
-class CountedWord:
-    """A word prepared to enter sentences accompanied by a quantity."""
-
-    def __init__ (self, singular_word, optional_plural = None):
-        """Init internal parameters.
-
-        If you don't give me an optional plural, I'll default to
-        appending an `s` to the end of the singular word.
-        """
-
-        self.__singular = singular_word
-        self.__figure_out_plural(optional_plural)
-
-    def __call__ (self, quantity):
-        """Return a string of the form `# word(s)`.
-
-        Whether we return the singular or the plural form of the word
-        depends on whether or not the quantity is 1.
-        """
-
-        if quantity == 1:
-            return self.__get_singular()
-
-        else:
-            return self.__get_plural(quantity)
-
-    def __repr__ (self):
-        """Return informataive representation of CountedWord object."""
-
-        return "<{} {}/{}>".format(
-                self.__class__.__name__,
-                self.__singular,
-                self.__plural)
-
-    def __figure_out_plural (self, optional_plural):
-        if optional_plural is None:
-            self.__set_default_plural()
-
-        else:
-            self.__set_specific_plural(optional_plural)
-
-    def __set_default_plural (self):
-        self.__plural = self.__singular + "s"
-
-    def __set_specific_plural (self, plural):
-        self.__plural = plural
-
-    def __get_singular (self):
-        return "1 " + self.__singular
-
-    def __get_plural (self, quantity):
-        return "{:d} {}".format(quantity, self.__plural)
 
 class TooManyArgumentsError (TypeError):
 
---
+++ falcom/general/test_counted_word.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2016 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+import unittest
+
+from .counted_word import *
+
+class TestCountedWord (unittest.TestCase):
+
+    def init_counted_word (self, *args):
+        self.word = CountedWord(*args)
+
+    def assert_getstr (self, number, rhs):
+        self.assertEqual(self.word(number), rhs)
+
+    def test_different_simple_nouns (self):
+        self.init_counted_word("thing")
+        self.assert_getstr(0, "0 things")
+        self.assert_getstr(1, "1 thing")
+        self.assert_getstr(2, "2 things")
+
+        self.init_counted_word("widget")
+        self.assert_getstr(0, "0 widgets")
+        self.assert_getstr(1, "1 widget")
+        self.assert_getstr(2, "2 widgets")
+
+    def test_custom_plural (self):
+        self.init_counted_word("octopus", "octopodes")
+        self.assert_getstr(0, "0 octopodes")
+        self.assert_getstr(1, "1 octopus")
+        self.assert_getstr(2, "2 octopodes")
+
+    def test_repr (self):
+        self.init_counted_word("beat")
+        self.assertEqual(repr(self.word), "<CountedWord beat/beats>")
+
+        self.init_counted_word("holler")
+        self.assertEqual(repr(self.word),
+                "<CountedWord holler/hollers>")
+
+        self.init_counted_word("forum", "fora")
+        self.assertEqual(repr(self.word), "<CountedWord forum/fora>")
+
+    def test_repr_on_child_class (self):
+        class SomeDerivativeOfCountedWord (CountedWord):
+            pass
+
+        w = SomeDerivativeOfCountedWord("rock")
+        self.assertEqual(repr(w),
+                "<SomeDerivativeOfCountedWord rock/rocks>")
