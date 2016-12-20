67b3926f7775ca6b6874e382a7d31bfa1f4ace81
Matthew Alexander LaChance <mattlach@umich.edu>
2016-12-20 13:12:56 -0500

    Move get_keys_required_by_format_str into its own file

--- falcom/volume_info/get_oclc_data.py
+++ falcom/volume_info/get_oclc_data.py
@@ -1,64 +1,17 @@
 # Copyright (c) 2016 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
-from collections.abc import Mapping
 from urllib.parse import urlencode
 from urllib.request import urlopen
 
 from ..exceptions import *
 from .tabular_data import TabularData
 from .argument_collector import ArgumentCollector
+from .str_format import get_keys_required_by_format_str
 
 ########################################################################
 ############################### Classes ################################
 ########################################################################
-
-class DecoyMapping (Mapping):
-    """Pretends to be a mapping but tracks queries and returns 0.
-
-    This is nice if you need to know exactly which keys are queried.
-    """
-
-    def __init__ (self, value = 0):
-        """Init an empty decoy mapping."""
-
-        # Start with an empty set.
-        self.__keys = set()
-        self.__value = value
-
-    def get_keys (self):
-        """Return the set of keys."""
-        return self.__keys
-
-    def __getitem__ (self, key):
-        """Add key and return 0."""
-
-        # A query! Add this to our set.
-        self.__keys.add(key)
-
-        # Return 0. Probably safe.
-        return self.__value
-
-    def __len__ (self):
-        """Return 0."""
-        return 0
-
-    def __iter__ (self):
-        """Return an empty iterator."""
-        return iter(())
-
-def extract_format_keys (s):
-    """Return the set of named keywords in the format string."""
-
-    # Use a decoy mapping to track whatever we're asking for.
-    mapping = DecoyMapping()
-
-    # We use format map to stop the thing from trying to convert to a
-    # dict or anything weird.
-    junk = s.format_map(mapping)
-
-    # Return the resulting set of keys.
-    return mapping.get_keys()
 
 class URI:
     """URI object that handles GET and/or POST data."""
@@ -75,7 +28,8 @@ class URI:
         # I don't want the arguments to iterate through any format names
         # that happen to be in the URI.
         self.__args.skip_iter(*tuple(
-                extract_format_keys(self.__uri.decode("ascii"))))
+                get_keys_required_by_format_str(
+                        self.__uri.decode("ascii"))))
 
     def get (self, *args, **kwargs):
         """Return an HTTPResponse for the given GET request."""
--- falcom/volume_info/test/test_get_oclc_data.py
+++ falcom/volume_info/test/test_get_oclc_data.py
@@ -10,27 +10,6 @@ from ..get_oclc_data import *
 SLOW_TESTS = os.environ.get("SLOW_TESTS", False)
 slow_test = unittest.skipUnless(SLOW_TESTS, "slow test")
 
-class TestDecoyMapping (unittest.TestCase):
-
-    def assert_keys (self, format_string, values):
-        """Run extract_format_keys and compare its result."""
-        self.assertEqual(extract_format_keys(format_string), values)
-
-    def test_format_empty (self):
-        self.assert_keys("no field names", set())
-
-    def test_format_one (self):
-        self.assert_keys("what if it's {hi}", {"hi"})
-
-    def test_format_multiple (self):
-        self.assert_keys("{hey} and {whatsup}", {"hey", "whatsup"})
-
-    def test_format_decimal (self):
-        self.assert_keys("{holler:d}", {"holler"})
-
-    def test_format_float (self):
-        self.assert_keys("{holler:f}", {"holler"})
-
 class TestURI (unittest.TestCase):
 
     def test_get (self):
