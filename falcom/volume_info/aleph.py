def3943357d4d99ec67d4a4b0795aa057e76a785
Matthew Alexander LaChance <mattlach@umich.edu>
2016-12-20 13:46:21 -0500

    Create aleph.MARCData

---
+++ falcom/volume_info/aleph.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2016 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+MARCData = None
---
+++ falcom/volume_info/test/test_aleph.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2016 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+import unittest
+
+from ..aleph import MARCData
