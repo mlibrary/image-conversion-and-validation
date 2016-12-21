24bbbbb0efc1d011372e3ddb86d69ce5e7a56c03
Matthew Alexander LaChance <mattlach@umich.edu>
2016-12-21 14:31:26 -0500

    Refactor URI file

--- falcom/volume_info/uri.py
+++ falcom/volume_info/uri.py
@@ -12,58 +12,36 @@ class URI:
 
     def __init__ (self, ArgumentCollector, *args, **kwargs):
         """Init URI with base, expected args, and default data."""
-
-        # The base URI should be the first positional argument.
-        self.__uri = self.__extract_uri(args)
-
-        # For everything else, we use an ArgumentCollector.
-        self.__args = ArgumentCollector(args[1:], kwargs)
-
-        # I don't want the arguments to iterate through any format names
-        # that happen to be in the URI.
-        self.__args.skip_iter(*tuple(
-                get_keys_required_by_format_str(
-                        self.__uri.decode("ascii"))))
+        self.__extract_uri(args)
+        self.__set_argument_collector(ArgumentCollector, args, kwargs)
+        self.__tell_collector_to_skip_format_keys()
 
     def get (self, *args, **kwargs):
         """Return an HTTPResponse for the given GET request."""
-
-        # We store URIs as bytes objects, so we need to decode it to an
-        # ASCII str.
         return urlopen(self.get_uri(*args, **kwargs).decode("ascii"))
 
     def post (self, *args, **kwargs):
         """Return an HTTPResponse for the given POST request."""
-
         uri, data = self.post_uri(*args, **kwargs)
 
-        # We store URIs as bytes objects, so we need to decode it to an
-        # ASCII str.
         return urlopen(uri.decode("ascii"), data)
 
     def get_uri (self, *args, **kwargs):
         """Return a URI bytes object with appropriate GET data."""
-
-        # Get the data.
         uri, data = self.post_uri(*args, **kwargs)
 
         if data:
-            # If we have any data, it's appended to the URI with a
-            # question mark.
-            return uri + b"?" + data
+            uri += b"?" + data
 
-        else:
-            # If we have no data, we can just use the URI as-is.
-            return uri
+        return uri
 
     def post_uri (self, *args, **kwargs):
         """Return a duple of bytes objects: base URI and POST data."""
-        # Get the data.
         data = self.__get_encoded_data(args, kwargs)
+        uri_str = self.__get_uri_str()
 
-        # Get the specific URI.
         uri = self.__uri.decode("ascii").format_map(
-                self.__args).encode("ascii")
+                self.__argument_collector).encode("ascii")
 
         return uri, data
 
@@ -75,7 +53,7 @@ class URI:
         if positional_args:
             # If we have any arguments, then we're looking at the first
             # one. It needs to be ASCII bytes.
-            return self.__assert_bytes(positional_args[0])
+            self.__uri = self.__assert_bytes(positional_args[0])
 
         else:
             # If we don't have any positional arguments, it's time to
@@ -89,16 +67,6 @@ class URI:
             # argument collector.
             raise MissingPositionalArgsError("__init__", 1, "uri_base")
 
-    def __get_encoded_data (self, args, kwargs):
-        # Update our internal argument collector.
-        self.__args.update(args, kwargs)
-
-        # Run urllib.parse.urlencode on the mapping.
-        encoded = urlencode(self.__args)
-
-        # We might get a str, and we want bytes.
-        return self.__assert_bytes(encoded)
-
     def __assert_bytes (self, ascii_str):
         if isinstance(ascii_str, bytes):
             # If it's already bytes, we'll just assume it's all ASCII.
@@ -107,3 +75,26 @@ class URI:
         else:
             # If it's a str, we'll need to encode it into bytes.
             return ascii_str.encode("ascii")
+
+    def __set_argument_collector (self,
+            ArgumentCollector, args, kwargs):
+        self.__argument_collector = ArgumentCollector(args[1:], kwargs)
+
+    def __tell_collector_to_skip_format_keys (self):
+        uri_str = self.__get_uri_str()
+        key_set = get_keys_required_by_format_str(uri_str)
+
+        self.__argument_collector.skip_iter(*tuple(key_set))
+
+    def __get_uri_str (self):
+        return self.__uri.decode("ascii")
+
+    def __get_encoded_data (self, args, kwargs):
+        # Update our internal argument collector.
+        self.__argument_collector.update(args, kwargs)
+
+        # Run urllib.parse.urlencode on the mapping.
+        encoded = urlencode(self.__argument_collector)
+
+        # We might get a str, and we want bytes.
+        return self.__assert_bytes(encoded)
