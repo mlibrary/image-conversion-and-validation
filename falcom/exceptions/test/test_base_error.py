fc991919eb2354578ca98e59b5ae304226e3a9b3
Matthew Alexander LaChance <mattlach@umich.edu>
2016-12-21 16:41:13 -0500

    Split exception tests into a couple files

---
+++ falcom/exceptions/test/test_base_error.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2016 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+import unittest
+
+from ..base_error import BaseError
+
+class TestBaseError (unittest.TestCase):
+
+    def test_is_exception (self):
+        self.assertTrue(issubclass(BaseError, Exception))
+
+    def test_zero_arg_error (self):
+        class ZeroArg (BaseError):
+            """test description"""
+            pass
+
+        error = ZeroArg()
+
+        self.assertIs(error.value, None)
+        self.assertEqual(str(error), "test description")
+
+    def test_one_arg_error (self):
+        class OneArg (BaseError):
+            """got {} is all"""
+            pass
+
+        matt = OneArg("matt")
+        self.assertEqual(matt.value, "matt")
+        self.assertEqual(str(matt), "got matt is all")
+
+        five = OneArg(5)
+        self.assertEqual(five.value, 5)
+        self.assertEqual(str(five), "got 5 is all")
+
+    def test_many_arg_error (self):
+        class ManyArg (BaseError):
+            """got {} and {}"""
+            pass
+
+        matt = ManyArg("matt", "he's cool")
+        self.assertEqual(matt.value, ("matt", "he's cool"))
+        self.assertEqual(str(matt), "got matt and he's cool")
+
+        five = ManyArg(5, 5)
+        self.assertEqual(five.value, (5, 5))
+        self.assertEqual(str(five), "got 5 and 5")
+
+    def test_newline_strip (self):
+        class ZeroArg (BaseError):
+            """some simple description
+
+            and some other info
+            """
+            pass
+
+        class OneArg (BaseError):
+            """something something {}
+
+            and some other info
+            """
+            pass
+
+        zero = ZeroArg()
+        self.assertIs(zero.value, None)
+        self.assertEqual(str(zero), "some simple description")
+
+        matt = OneArg("matt")
+        self.assertEqual(matt.value, "matt")
+        self.assertEqual(str(matt), "something something matt")
+
+        five = OneArg(5)
+        self.assertEqual(five.value, 5)
+        self.assertEqual(str(five), "something something 5")
