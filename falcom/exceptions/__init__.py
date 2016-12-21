efd37229a86ee63f2af094d1797867e4a8b3b302
Matthew Alexander LaChance <mattlach@umich.edu>
2016-12-21 16:32:20 -0500

    Split different types of base exceptions

---
+++ falcom/exceptions/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2016 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+from .base_error import BaseError
+from .type_errors import MissingPositionalArgsError, \
+        MultipleValuesOneArgError, \
+        TooManyArgumentsError
