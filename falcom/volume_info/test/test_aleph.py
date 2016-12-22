9c681c6375d5dcac48c966163efd9701ed67aa45
Matthew Alexander LaChance <mattlach@umich.edu>
2016-12-22 10:41:44 -0500

    Refactor aleph tests

--- falcom/volume_info/test/test_aleph.py
+++ falcom/volume_info/test/test_aleph.py
@@ -16,19 +16,23 @@ def readfile (filename):
 
 EG_MARC_ISMAN = readfile("39015079130699.xml")
 EG_MARC_ASTRO = readfile("39015081447313.xml")
-EG_MARC_BUSNS = readfile("39015090867675.xml")
+EG_MARC_BUSINESS = readfile("39015090867675.xml")
 
 class TestMARCData (unittest.TestCase):
 
+    def assert_marc_values (self, xml_data, **kwargs):
+        marc = MARCData(xml_data)
+
+        for key, value in kwargs.items():
+            self.assertEqual(getattr(marc, key), value)
+
     def test_degenerate (self):
-        isman = MARCData(EG_MARC_ISMAN)
-        astro = MARCData(EG_MARC_ASTRO)
+        self.assert_marc_values(EG_MARC_ISMAN,
+                bib="006822264",
+                callno="Isl. Ms. 402",
+                author="")
 
-        self.assertEqual(isman.bib, "006822264")
-        self.assertEqual(astro.bib, "002601791")
-
-        self.assertEqual(isman.callno, "Isl. Ms. 402")
-        self.assertEqual(astro.callno, "Isl. Ms. 782")
-
-        self.assertEqual(isman.author, "")
-        self.assertEqual(astro.author, "")
+        self.assert_marc_values(EG_MARC_ASTRO,
+                bib="002601791",
+                callno="Isl. Ms. 782",
+                author="")
