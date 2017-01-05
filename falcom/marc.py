0439de78fdcb92b0664179eb28d17a988b7eca0b
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-05 16:17:38 -0500

    Starting over with more-explicit MARCData structure

--- falcom/marc.py
+++ falcom/marc.py
@@ -1,13 +1,12 @@
 # Copyright (c) 2017 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
+class MARCData:
+
+    bib = None
+
+    def __bool__ (self):
+        return False
+
 def get_marc_data_from_xml (xml):
-    return {
-            "bib": None,
-            "callno": None,
-            "oclc": None,
-            "author": None,
-            "title": None,
-            "description": None,
-            "years": (None, None),
-    }
+    return MARCData()
--- falcom/test_marc.py
+++ falcom/test_marc.py
@@ -5,20 +5,14 @@ from hamcrest import *
 import unittest
 
 from .marc import *
-from .hamcrest_marc import contains_marc_fields
 
 class NothingTest (unittest.TestCase):
 
-    def test_marc_data_of_None_yields_None (self):
+    def test_marc_data_of_None_yields_empty_MARC_data (self):
         marc_data = get_marc_data_from_xml(None)
-        assert_that(marc_data, contains_marc_fields())
 
-        for key, value in marc_data.items():
-            if key == "years":
-                assert_that(value, is_(equal_to((None, None))))
-
-            else:
-                assert_that(value, is_(none()))
+        assert_that(bool(marc_data), is_(equal_to(False)))
+        assert_that(marc_data.bib, is_(none()))
 
 if __name__ == "__main__":
     unittest.main()
