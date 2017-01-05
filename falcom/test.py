82abdd034445ba4a8bf5230f019859be43f1f3c8
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-05 13:12:52 -0500

    Working test environment

---
+++ falcom/test.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2017 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+from hamcrest import *
+import unittest
+
+class NothingTest (unittest.TestCase):
+
+    def test_nothing (self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
