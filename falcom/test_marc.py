784b3b06c55ea84826a46e6025664ee4ac8befc3
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-06 10:59:35 -0500

    Create composed hamcrest assertion for empty marc data

--- falcom/test_marc.py
+++ falcom/test_marc.py
@@ -5,23 +5,33 @@ from hamcrest import *
 import unittest
 
 from .marc import *
-from .hamcrest_marc import has_marc_attrs, \
-        evaluates_to_true, evaluates_to_false
+from .hamcrest_marc import ComposedAssertion, \
+        has_marc_attrs, evaluates_to_true, evaluates_to_false
 
-class NothingTest (unittest.TestCase):
+class IsEmptyMarcData (ComposedAssertion):
 
-    def test_marc_data_of_None_yields_empty_MARC_data (self):
-        marc_data = get_marc_data_from_xml(None)
-
-        assert_that(marc_data, evaluates_to_false())
-        assert_that(marc_data, has_marc_attrs(
+    def assertion (self, item):
+        yield evaluates_to_false()
+        yield has_marc_attrs(
                 bib=None,
                 callno=None,
                 oclc=None,
                 author=None,
                 title=None,
                 description=None,
-                years=(None, None)))
+                years=(None, None))
+
+def is_empty_marc_data(): return IsEmptyMarcData()
+
+class NothingTest (unittest.TestCase):
+
+    def test_marc_data_of_None_yields_empty_MARC_data (self):
+        marc_data = get_marc_data_from_xml(None)
+        assert_that(marc_data, is_empty_marc_data())
+
+    def test_marc_data_of_empty_str_yields_empty_MARC_data (self):
+        marc_data = get_marc_data_from_xml("")
+        assert_that(marc_data, is_empty_marc_data())
 
 if __name__ == "__main__":
     unittest.main()
