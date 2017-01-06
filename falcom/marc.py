1f64b1de9e4ac35a0cf0308a7471b548b40f40c9
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-06 14:04:53 -0500

    Actually parsing XML to get data

--- falcom/marc.py
+++ falcom/marc.py
@@ -1,6 +1,9 @@
 # Copyright (c) 2017 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
+import re
+import xml.etree.ElementTree as ET
+
 class MARCData:
 
     @property
@@ -33,16 +36,59 @@ class MARCData:
 
     def __init__ (self, **kwargs):
         self.__marc_dict = kwargs
+        null_keys = [k for k, v in kwargs.items() if v is None]
+        for key in null_keys:
+            del kwargs[key]
 
     def __bool__ (self):
         return bool(self.__marc_dict)
 
 def get_marc_data_from_xml (xml):
-    if xml is not None and len(xml) > 0:
-        return MARCData(bib="006822264",
-                        callno="Isl. Ms. 402",
-                        title="[Calligraphic specimen,",
-                        years=("1790", "1791"))
-
-    else:
+    if xml is None:
         return MARCData()
+
+    if not isinstance(xml, ET.Element):
+        try:
+            xml = ET.fromstring(xml)
+
+        except ET.ParseError:
+            return MARCData()
+
+    marc = { }
+    xmlns = "http://www.loc.gov/MARC21/slim"
+
+    marc["bib"] = getattr(
+            xml.find(".//{{{}}}controlfield[@tag='001']".format(xmlns)),
+            "text", None)
+
+    marc["callno"] = getattr(
+            xml.find(".//{{{xmlns}}}datafield[@tag='MDP']/" \
+                     "{{{xmlns}}}subfield[@code='h']".format(
+                            xmlns=xmlns)),
+            "text", None)
+
+    marc["title"] = getattr(
+            xml.find(".//{{{xmlns}}}datafield[@tag='245']/" \
+                     "{{{xmlns}}}subfield[@code='a']".format(
+                            xmlns=xmlns)),
+            "text", None)
+
+    oclcs = xml.findall(".//{{{xmlns}}}datafield[@tag='035']/" \
+                     "{{{xmlns}}}subfield[@code='a']".format(
+                            xmlns=xmlns))
+
+    for maybe in oclcs:
+        match = re.match(r"^\(OCoLC\).*?([0-9]+)$", maybe.text)
+        if match:
+            marc["oclc"] = match.group(1)
+            break
+
+    years = xml.find(".//{{{}}}controlfield[@tag='008']".format(xmlns))
+
+    if years is not None:
+        year1, year2 = years.text[7:11], years.text[11:15]
+        if year1 == "^^^^": year1 = None
+        if year2 == "^^^^": year2 = None
+        marc["years"] = (year1, year2)
+
+    return MARCData(**marc)
--- falcom/test/test_marc.py
+++ falcom/test/test_marc.py
@@ -77,5 +77,33 @@ class MARCDataTest (unittest.TestCase):
                                   years=("1790", "1791"),
                                   oclc=None))
 
+    def test_correct_marc_data_from_astro_xml (self):
+        assert_that(get_marc_data_from_xml(EG_MARC_ASTRO),
+                    has_marc_data(bib="002601791",
+                                  callno="Isl. Ms. 782",
+                                  author=None,
+                                  title="Astronomical tables :",
+                                  description=None,
+                                  years=("16uu", None),
+                                  oclc="706055947"))
+
+    def later (self):
+        self.assert_marc_values(EG_MARC_BUSINESS,
+                bib="011694516",
+                callno="LD755.A87 P45 2012",
+                author="Pelfrey, Patricia A.",
+                title="Entrepreneurial president :",
+                description=None,
+                years=("2012", None),
+                oclc="756167029")
+        self.assert_marc_values(EG_MARC_MIDAILY,
+                bib="002751011",
+                callno="FImu F3g Outsize",
+                author=None,
+                title="The Michigan daily.",
+                description="1927 Sept 20 - 1928 Jan 8",
+                years=("1903", "9999"),
+                oclc="009651208")
+
 if __name__ == "__main__":
     unittest.main()
