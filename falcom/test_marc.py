2113622fccfc962048f98feeacb7ef5b6a0e8e9d
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-06 12:33:54 -0500

    Refactor is_empty_marc_data assertion

--- falcom/test_marc.py
+++ falcom/test_marc.py
@@ -8,20 +8,30 @@ from .marc import *
 from .hamcrest_marc import ComposedAssertion, \
         has_marc_attrs, evaluates_to_true, evaluates_to_false
 
-class IsEmptyMarcData (ComposedAssertion):
+class IsParticularMarcData (ComposedAssertion):
+
+    def __init__ (self, eval_to_true = True, **kwargs):
+        self.eval_to_true = eval_to_true
+        self.kwargs = kwargs
 
     def assertion (self, item):
-        yield evaluates_to_false()
-        yield has_marc_attrs(
-                bib=None,
-                callno=None,
-                oclc=None,
-                author=None,
-                title=None,
-                description=None,
-                years=(None, None))
+        if self.eval_to_true:
+            yield evaluates_to_true()
 
-def is_empty_marc_data(): return IsEmptyMarcData()
+        else:
+            yield evaluates_to_false()
+
+        yield has_marc_attrs(**self.kwargs)
+
+def is_empty_marc_data():
+    return IsParticularMarcData(False,
+                                bib=None,
+                                callno=None,
+                                oclc=None,
+                                author=None,
+                                title=None,
+                                description=None,
+                                years=(None, None))
 
 class NothingTest (unittest.TestCase):
 
