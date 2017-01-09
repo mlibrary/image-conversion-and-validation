2bc89e470d6e1bcf29d689424c9db0f5c197e33d
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-09 16:20:11 -0500

    Add test file for worldcat DSs

---
+++ falcom/test/test_worldcat.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2017 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+from hamcrest import *
+import os
+import unittest
+
+FILE_BASE = os.path.join(os.path.dirname(__file__), "files")
+
+def readfile (filename):
+    with open(os.path.join(FILE_BASE, filename), "r") as f:
+        result = f.read()
+
+    return result
+
+EG_OCLC_ASTRO = readfile("worldcat-706055947.json")
+EG_OCLC_BUSINESS = readfile("worldcat-756167029.json")
+EG_OCLC_MIDAILY = readfile("worldcat-009651208.json")
+
+class Nothing (unittest.TestCase):
+
+    def test_nothing (self):
+        pass
