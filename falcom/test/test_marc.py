e423d4a17cc34fca7f8062329638685eb59cf767
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-10 11:42:23 -0500

    Call get_marc_data_from_xml in composed assert

--- falcom/test/test_marc.py
+++ falcom/test/test_marc.py
@@ -30,101 +30,99 @@ EG_MARC_AUTHOR_130 = readfile("author_130_39015090867675.xml")
 def has_marc_attrs(**kwargs):
     return HasAttrs("MARC attrs", **kwargs)
 
-class IsParticularMarcData (ComposedAssertion):
+class YieldsParticularMarcData (ComposedAssertion):
 
     def __init__ (self, eval_to_true = True, **kwargs):
         self.eval_to_true = eval_to_true
         self.kwargs = kwargs
 
     def assertion (self, item):
+        data = get_marc_data_from_xml(item)
+
         if self.eval_to_true:
-            yield evaluates_to_true()
+            yield data, evaluates_to_true()
 
         else:
-            yield evaluates_to_false()
+            yield data, evaluates_to_false()
 
-        yield has_marc_attrs(**self.kwargs)
+        yield data, has_marc_attrs(**self.kwargs)
 
-def has_marc_data(**kwargs):
-    return IsParticularMarcData(True, **kwargs)
+def yields_marc_data(**kwargs):
+    return YieldsParticularMarcData(True, **kwargs)
 
-def is_empty_marc_data():
-    return IsParticularMarcData(False,
-                                bib=None,
-                                callno=None,
-                                oclc=None,
-                                author=None,
-                                title=None,
-                                description=None,
-                                years=(None, None))
+def yields_empty_marc_data():
+    return YieldsParticularMarcData(False,
+                                    bib=None,
+                                    callno=None,
+                                    oclc=None,
+                                    author=None,
+                                    title=None,
+                                    description=None,
+                                    years=(None, None))
 
 class MARCDataTest (unittest.TestCase):
 
     def test_marc_data_of_None_yields_empty_MARC_data (self):
-        assert_that(get_marc_data_from_xml(None),
-                    is_empty_marc_data())
+        assert_that(None, yields_empty_marc_data())
 
     def test_marc_data_of_empty_str_yields_empty_MARC_data (self):
-        assert_that(get_marc_data_from_xml(""),
-                    is_empty_marc_data())
+        assert_that("", yields_empty_marc_data())
 
     def test_marc_data_of_empty_xml_yields_empty_MARC_data (self):
         empty_etree = ET.fromstring("<empty/>")
-
-        assert_that(get_marc_data_from_xml(empty_etree),
-                    is_empty_marc_data())
+        assert_that(empty_etree, yields_empty_marc_data())
 
     def test_correct_marc_data_from_isman_xml (self):
-        assert_that(get_marc_data_from_xml(EG_MARC_ISMAN),
-                    has_marc_data(bib="006822264",
-                                  callno="Isl. Ms. 402",
-                                  author=None,
-                                  title="[Calligraphic specimen,",
-                                  description=None,
-                                  years=("1790", "1791"),
-                                  oclc=None))
+        assert_that(EG_MARC_ISMAN, yields_marc_data(
+                        bib="006822264",
+                        callno="Isl. Ms. 402",
+                        author=None,
+                        title="[Calligraphic specimen,",
+                        description=None,
+                        years=("1790", "1791"),
+                        oclc=None))
 
     def test_correct_marc_data_from_astro_xml (self):
-        assert_that(get_marc_data_from_xml(EG_MARC_ASTRO),
-                    has_marc_data(bib="002601791",
-                                  callno="Isl. Ms. 782",
-                                  author=None,
-                                  title="Astronomical tables :",
-                                  description=None,
-                                  years=("16uu", None),
-                                  oclc="706055947"))
+        assert_that(EG_MARC_ASTRO, yields_marc_data(
+                        bib="002601791",
+                        callno="Isl. Ms. 782",
+                        author=None,
+                        title="Astronomical tables :",
+                        description=None,
+                        years=("16uu", None),
+                        oclc="706055947"))
 
     def test_correct_marc_data_from_business_xml (self):
-        assert_that(get_marc_data_from_xml(EG_MARC_BUSINESS),
-                    has_marc_data(bib="011694516",
-                                  callno="LD755.A87 P45 2012",
-                                  author="Pelfrey, Patricia A.",
-                                  title="Entrepreneurial president :",
-                                  description=None,
-                                  years=("2012", None),
-                                  oclc="756167029"))
+        assert_that(EG_MARC_BUSINESS, yields_marc_data(
+                        bib="011694516",
+                        callno="LD755.A87 P45 2012",
+                        author="Pelfrey, Patricia A.",
+                        title="Entrepreneurial president :",
+                        description=None,
+                        years=("2012", None),
+                        oclc="756167029"))
 
     def test_correct_marc_data_from_midaily_xml (self):
-        assert_that(get_marc_data_from_xml(EG_MARC_MIDAILY),
-                    has_marc_data(bib="002751011",
-                                  callno="FImu F3g Outsize",
-                                  author=None,
-                                  title="The Michigan daily.",
-                                  description="1927 Sept 20 - 1928 Jan 8",
-                                  years=("1903", "9999"),
-                                  oclc="009651208"))
+        assert_that(EG_MARC_MIDAILY, yields_marc_data(
+                        bib="002751011",
+                        callno="FImu F3g Outsize",
+                        author=None,
+                        title="The Michigan daily.",
+                        description="1927 Sept 20 - 1928 Jan 8",
+                        years=("1903", "9999"),
+                        oclc="009651208"))
 
     def test_author_can_pull_from_datafield_110 (self):
-        assert_that(get_marc_data_from_xml(EG_MARC_AUTHOR_110),
-                    has_marc_data(author="Chiusi. Museo Etrusco."))
+        assert_that(EG_MARC_AUTHOR_110, yields_marc_data(
+                        author="Chiusi. Museo Etrusco."))
 
     def test_author_can_pull_from_datafield_111 (self):
-        assert_that(get_marc_data_from_xml(EG_MARC_AUTHOR_111),
-                    has_marc_data(author="Pelfrey, Patricia A."))
+        assert_that(EG_MARC_AUTHOR_111, yields_marc_data(
+                        author="Pelfrey, Patricia A."))
 
     def test_author_can_pull_from_datafield_130 (self):
-        assert_that(get_marc_data_from_xml(EG_MARC_AUTHOR_130),
-                    has_marc_data(author="Pelfrey, Patricia A."))
+        assert_that(EG_MARC_AUTHOR_130, yields_marc_data(
+                        author="Pelfrey, Patricia A."))
 
 if __name__ == "__main__":
     unittest.main()
