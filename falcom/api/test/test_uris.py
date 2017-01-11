6e1e49a909a0ac31845723629113a65a06d53d0d
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-11 08:46:14 -0500

    Begin testing URIs

---
+++ falcom/api/test/test_uris.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2017 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+from hamcrest import *
+import unittest
+
+from .hamcrest import ComposedAssertion
+
+# There are three URIs that I need to use:
+#
+# http://catalog.hathitrust.org/api/volumes/brief/oclc/[OCLC].json
+# http://mirlyn-aleph.lib.umich.edu/cgi-bin/bc2meta?id=[BARCODE]&type=bc&schema=marcxml
+# http://www.worldcat.org/webservices/catalog/content/libraries/[OCLC]?wskey=[WC_KEY]&format=json&maximumLibraries=50
+
+class NothingTest (unittest.TestCase):
+
+    def test_nothing (self):
+        pass
