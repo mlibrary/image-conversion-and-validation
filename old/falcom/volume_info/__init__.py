a9847213075829ebcebbca361dac3cce7238f92e
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-12 15:23:38 -0500

    Get rid of old stuff

--- old/falcom/volume_info/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) 2016 The Regents of the University of Michigan.
-# All Rights Reserved. Licensed according to the terms of the Revised
-# BSD License. See LICENSE.txt for details.
--- old/falcom/volume_info/tabular_data/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2016 The Regents of the University of Michigan.
-# All Rights Reserved. Licensed according to the terms of the Revised
-# BSD License. See LICENSE.txt for details.
-
-from .base import TabularData
-from .filesystem import TabularDataFromFilePath
