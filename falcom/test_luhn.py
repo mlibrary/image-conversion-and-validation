24fb2cfddb8bf380eed13710183d153799139fa6
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-25 16:09:33 -0500

    New test file for luhn check digits

---
+++ falcom/test_luhn.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2017 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+from hamcrest import *
+import unittest
+
+class Nothing (unittest.TestCase):
+
+    def test_working_test_environment (self):
+        pass
