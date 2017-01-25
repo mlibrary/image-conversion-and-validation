e03eaddd756a5961e97ee8d15cb0a0bff6ea0a22
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-25 16:20:52 -0500

    Oops, forgot to run git add

---
+++ falcom/luhn.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2017 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+def get_check_digit (number = None):
+    pass
