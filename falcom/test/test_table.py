28ce2cadd72bd97a2825decca676ae229f4f101b
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-27 16:24:24 -0500

    Ready to test tables

---
+++ falcom/test/test_table.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2017 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+from hamcrest import *
+import unittest
+
+from .hamcrest import ComposedAssertion
+
+class TableTest (unittest.TestCase):
+
+    def test_working_environment (self):
+        pass
