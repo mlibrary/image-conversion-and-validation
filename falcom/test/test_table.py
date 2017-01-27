3428c7dc18080bd6f6fc77c7104dbfb3ac62f52f
Matthew Alexander LaChance <mattlach@umich.edu>
2017-01-27 16:47:22 -0500

    Refactor into composed matcher

--- falcom/test/test_table.py
+++ falcom/test/test_table.py
@@ -8,14 +8,19 @@ from .hamcrest import ComposedAssertion, \
         evaluates_to_false, evaluates_to_true
 from ..table import Table
 
+class an_empty_table (ComposedAssertion):
+
+    def assertion (self, item):
+        yield item, evaluates_to_false()
+        yield item, has_length(0)
+        yield item.rows, is_(equal_to(0))
+        yield item.cols, is_(equal_to(0))
+        yield list(item), is_(equal_to([]))
+        yield calling(lambda t: t[0]).with_args(item), \
+              raises(IndexError)
+
 class TableTest (unittest.TestCase):
 
     def test_degenerate (self):
         table = Table()
-        assert_that(table, evaluates_to_false())
-        assert_that(table, has_length(0))
-        assert_that(table.rows, is_(equal_to(0)))
-        assert_that(table.cols, is_(equal_to(0)))
-        assert_that(list(table), is_(equal_to([])))
-        assert_that(calling(lambda t: t[0]).with_args(table),
-                    raises(IndexError))
+        assert_that(table, is_(an_empty_table()))
