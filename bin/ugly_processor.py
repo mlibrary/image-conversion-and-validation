f0d9854799a5bbd13f8797f899ebb2640c93ca9d
Matthew Alexander LaChance <mattlach@umich.edu>
2017-02-03 09:50:33 -0500

    Add a lot of validation for incoming spreadsheets

--- bin/ugly_processor.py
+++ bin/ugly_processor.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
 from collections import namedtuple
 from falcom.api.reject_list import VolumeDataFromBarcode
+from re import compile as re_compile
+
+RE_14_BARCODE = re_compile(r"^[0-9]{14}$")
+RE_PROTO_BARCODE = re_compile(r"^[Bb][0-9]+$")
 
 UM_INSTITUTIONS = [
   "EYM",
@@ -155,4 +160,43 @@ REQUIRED_FIELDS = {
     "year1":  "dates",
 }
 
-print(repr(header_row))
+parser = ArgumentParser(description="Extend spreadsheets")
+parser.add_argument("spreadsheets", nargs="+")
+args = parser.parse_args()
+
+tables = [ ]
+
+for spreadsheet in spreadsheets:
+    with open(spreadsheet, "r") as f:
+        data = f.read().rstrip("\n")
+
+    assert "\r" not in data, \
+            "couldn't figure out newlines " + spreadsheet
+
+    table = [r.split("\t") for r in data.split("\n") if r.strip("\t")]
+    assert table, "empty table " + spreadsheet
+
+    cols = len(table[0])
+    for row in table[1:]:
+        assert len(row) == cols, \
+                "inconsistent column counts " + spreadsheet
+
+    maybe_a_barcode = table[0][0]
+    match = RE_14_BARCODE.match(maybe_a_barcode)
+
+    if match is None:
+        match = RE_PROTO_BARCODE.match(maybe_a_barcode)
+
+    if match is not None:
+        table.insert(0, [""] * cols)
+
+    for row in table[1:]:
+        assert row[-1] in {"DC", "DX", "DY", "DZ"}, \
+                "invalid status {} in {}".format(repr(row[-1]),
+                                                 spreadsheet)
+
+    tables.append(table)
+
+for table in tables:
+    for row in table[1:]:
+        print("{}\t{}".format(row[0], row[-1]))
