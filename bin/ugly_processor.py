0fa6463997aaab0bae542ff1d1c4cda9c8379124
Matthew Alexander LaChance <mattlach@umich.edu>
2017-02-03 10:48:59 -0500

    Draft of ugly processor

--- bin/ugly_processor.py
+++ bin/ugly_processor.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
 from collections import namedtuple
+from datetime import datetime
 from falcom.api.reject_list import VolumeDataFromBarcode
 from re import compile as re_compile
 
 RE_14_BARCODE = re_compile(r"^[0-9]{14}$")
 RE_PROTO_BARCODE = re_compile(r"^[Bb][0-9]+$")
 
-UM_INSTITUTIONS = [
+UM_INSTITUTIONS = {
   "EYM",
   "BEU",
   "E8W",
@@ -20,13 +21,13 @@ UM_INSTITUTIONS = [
   "U2T",
   "UMSPO",
   "UMDON",
-]
+}
 
-HATHI_INSTITUTIONS = [
+HATHI_INSTITUTIONS = {
   "HATHI",
-]
+}
 
-CIC_INSTITUTIONS = [
+CIC_INSTITUTIONS = {
   # University of Chicago
   "CGU",
   "IAB",
@@ -117,7 +118,7 @@ CIC_INSTITUTIONS = [
 
   # University of Wisconsin - Madison
   "GZI",
-]
+}
 
 DataRow = namedtuple("DataRow",
                      ("bib",
@@ -196,3 +197,73 @@ for spreadsheet in args.spreadsheets:
                                                  spreadsheet)
 
     tables[spreadsheet] = table
+
+barcode_filename = datetime.now().strftime("barcode-%Y%m%d.txt")
+
+for filename, table in tables.items():
+    print("Processing {} ...".format(filename))
+    new_table = [table[0] + list(header_row)]
+    barcode_lines = [ ]
+
+    for i in range(1, len(table)):
+        row = table[i]
+        barcode = row[0]
+        status = row[-1]
+
+        print("  {:< 14s} ({:d}/{:d}) ...".format(
+                        barcode, i, len(table) - 1))
+
+        data = VolumeDataFromBarcode(barcode)
+
+        if data.marc:
+            numcic = 0
+            numoth = 0
+            numum = 0
+            dumb = 0
+            for code in data.worldcat:
+                if code in HATHI_INSTITUTIONS:
+                    dumb += 1
+                elif code in UM_INSTITUTIONS:
+                    numum += 1
+                elif code in CIC_INSTITUTIONS:
+                    numcic += 1
+                else:
+                    numoth += 1
+
+            if numcic > 0:
+                is_unique = numcic + numoth < 3
+
+            else:
+                is_unique = numcic + numoth < 5
+
+            extension = DataRow(
+                    data.marc.bib,
+                    data.marc.oclc,
+                    data.marc.callno,
+                    data.marc.author,
+                    data.marc.title,
+                    data.marc.description,
+                    data.marc.years[0],
+                    data.marc.years[1],
+                    "unique" if is_unique else "",
+                    "{:d}".format(numcic),
+                    "{:d}".format(numoth),
+                    "{:d}".format(numum),
+                    "{:d}".format(dumb),
+                    "{:d}".format(data.oclc_counts[0]),
+                    "{:d}".format(data.oclc_counts[1]))
+
+            new_row = row + list(extension)
+            new_table.append("" if x is None else x
+                             for x in new_row)
+
+            barcode_lines.append("{}\t{}\n".format(barcode, status))
+
+    with open(filename, "w") as spreadsheet_file:
+        for row in new_table:
+            spreadsheet_file.write("\t".join(row) + "\n")
+
+    with open(barcode_filename, "a") as barcode_file:
+        barcode_file.write("".join(barcode_lines))
+
+print("Done.")
