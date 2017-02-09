e10e0fbdd0386fe5eae1cec3f10031aca441a216
Matthew Alexander LaChance <mattlach@umich.edu>
2017-02-09 16:57:59 -0500

    Extract __try_to_get() method

--- falcom/api/uri/api_querier.py
+++ falcom/api/uri/api_querier.py
@@ -5,18 +5,32 @@ from time import sleep
 
 class APIQuerier:
 
+    class __special_null: pass
+
     def __init__ (self, uri, url_opener, sleep_time=300, max_tries=0):
         self.uri = uri
         self.url_opener = url_opener
         self.sleep_time = sleep_time
         self.max_tries = max_tries
 
-    def get (self, **kwargs):
-        class SpecialNull: pass
-        self.result = SpecialNull
+    def get (self, **kwargs): # not thread-safe :(
+        self.result = self.__special_null
         self.attempt_number = 1
 
-        while self.result is SpecialNull:
+        self.__try_to_get(kwargs)
+
+        return self.result
+
+    @staticmethod
+    def utf8 (str_or_bytes):
+        if isinstance(str_or_bytes, bytes):
+            return str_or_bytes.decode("utf_8")
+
+        else:
+            return str_or_bytes
+
+    def __try_to_get (self, kwargs):
+        while self.result is self.__special_null:
             try:
                 self.result = self.__open_uri(kwargs)
 
@@ -28,16 +42,6 @@ class APIQuerier:
 
                 else:
                     self.attempt_number += 1
-
-        return self.result
-
-    @staticmethod
-    def utf8 (str_or_bytes):
-        if isinstance(str_or_bytes, bytes):
-            return str_or_bytes.decode("utf_8")
-
-        else:
-            return str_or_bytes
 
     def __open_uri (self, kwargs):
         with self.url_opener(self.uri(**kwargs)) as response:
