aaaaee572e45f0dfc7a06315d5a1bb29e8d07154
Matthew Alexander LaChance <mattlach@umich.edu>
2017-02-16 16:25:47 -0500

    Split up test

--- falcom/test/test_config.py
+++ falcom/test/test_config.py
@@ -13,8 +13,10 @@ class GivenEmptyConfig (unittest.TestCase):
     def setUp (self):
         self.config = Config()
 
-    def test_can_init_config (self):
+    def test_evaluates_to_false (self):
         assert_that(self.config, evaluates_to_false())
+
+    def test_has_zero_length (self):
         assert_that(self.config, has_length(0))
 
     def test_default_key_is_default (self):
