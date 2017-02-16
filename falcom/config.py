2e05f2f9290320c4f9a5b7e516e5dddb65b7dc57
Matthew Alexander LaChance <mattlach@umich.edu>
2017-02-16 16:15:00 -0500

    Add repr(Config)

--- falcom/config.py
+++ falcom/config.py
@@ -1,5 +1,8 @@
 # Copyright (c) 2017 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
+
 class Config:
-    pass
+
+    def __repr__ (self):
+        return "<{}>".format(self.__class__.__name__)
