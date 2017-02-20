aaaa7f040db7438f0f674c74bc6b0de90b42bb70
Matthew Alexander LaChance <mattlach@umich.edu>
2017-02-20 11:50:35 -0500

    Use new format for HathiData

--- falcom/api/hathi/data.py
+++ falcom/api/hathi/data.py
@@ -8,16 +8,13 @@ from ..common.read_only_data_structure import ReadOnlyDataStructure
 
 class HathiData (ReadOnlyDataStructure):
 
+    auto_properties = (
+        ("titles", ()),
+        ("htids", ()),
+    )
+
     __re_symbol = re_compile(r"[^0-9A-Za-z\s]")
     __re_spaces = re_compile(r"\s+")
-
-    @property
-    def titles (self):
-        return self.get("titles", ())
-
-    @property
-    def htids (self):
-        return self.get("htids", ())
 
     def get_item_counts (self, htid):
         matching_count = len([x for x in self.htids if x == htid])
