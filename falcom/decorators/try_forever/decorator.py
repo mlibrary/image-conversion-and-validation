aaaaa3be3f0642697f296ea8ad8cf22908aef617
Matthew Alexander LaChance <mattlach@umich.edu>
2017-03-20 16:49:49 -0400

    Move the decorator into its own file

--- falcom/decorators/try_forever/decorator.py
+++ falcom/decorators/try_forever/decorator.py
@@ -2,4 +2,13 @@
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
 
-from .try_forever import try_forever
+from .try_forever import TryForever
+
+def try_forever (*args, **kwargs):
+    obj = TryForever(kwargs)
+
+    if args:
+        return obj(*args)
+
+    else:
+        return obj
