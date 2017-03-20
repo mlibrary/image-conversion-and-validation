aaaaac5283927b60ec538e139a2e44527c26bd9d
Matthew Alexander LaChance <mattlach@umich.edu>
2017-03-20 13:38:13 -0400

    Extract method

--- falcom/test/read_example_file.py
+++ falcom/test/read_example_file.py
@@ -7,11 +7,12 @@ from unittest import TestCase
 class ExampleFileTest (TestCase):
 
     def setUp (self):
-        format_str = getattr(self, "format_str", "{}")
-        full_filename = format_str.format(self.filename)
-
         files_dir = join(dirname(self.this__file__), "files")
-        file_path = join(files_dir, full_filename)
+        file_path = join(files_dir, self.__full_filename())
 
         with open(file_path, "r") as f:
             self.file_data = f.read()
+
+    def __full_filename (self):
+        format_str = getattr(self, "format_str", "{}")
+        return format_str.format(self.filename)
