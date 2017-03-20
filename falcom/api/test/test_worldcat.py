aaaaa7e2735cd2adbc9b2e270703ef237e0a469c
Matthew Alexander LaChance <mattlach@umich.edu>
2017-03-20 14:59:02 -0400

    Remove unused code

--- falcom/api/test/test_worldcat.py
+++ falcom/api/test/test_worldcat.py
@@ -2,24 +2,11 @@
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
 from hamcrest import *
-import os
 import unittest
 
 from ...test.hamcrest import ComposedMatcher, evaluates_to
 from ...test.read_example_file import ExampleFileTest
 from ..worldcat import *
-
-FILE_BASE = os.path.join(os.path.dirname(__file__), "files")
-
-def readfile (filename):
-    with open(os.path.join(FILE_BASE, filename), "r") as f:
-        result = f.read()
-
-    return result
-
-EG_OCLC_ASTRO = readfile("worldcat-706055947.json")
-EG_OCLC_BUSINESS = readfile("worldcat-756167029.json")
-EG_OCLC_MIDAILY = readfile("worldcat-009651208.json")
 
 class yields_empty_worldcat_data (ComposedMatcher):
 
