aaaaaa747fad0e0ac66190fa0853572681f9443c
Matthew Alexander LaChance <mattlach@umich.edu>
2017-03-20 13:20:25 -0400

    Write TestCase subclass that reads an example file

---
+++ falcom/test/read_example_file.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2017 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+from os.path import join, dirname
+from unittest import TestCase
+
+class ExampleFileTest (TestCase):
+
+    def setUp (self):
+        format_str = getattr(self, "format_str", "{}")
+        full_filename = format_str.format(self.filename)
+
+        files_dir = join(dirname(__file__), "files")
+        file_path = join(files_dir, full_filename)
+
+        with open(file_path, "r") as f:
+            self.file_data = f.read()
