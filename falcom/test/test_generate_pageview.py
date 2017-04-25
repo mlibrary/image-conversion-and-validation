aaaa4198dcd592c2bf61ae3c4f789c959c642a90
Matthew Alexander LaChance <mattlach@umich.edu>
2017-04-25 11:32:07 -0400

    :white_check_mark: Add generate_pageview.Pagetags

---
+++ falcom/test/test_generate_pageview.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2017 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+from hamcrest import *
+import unittest
+
+from ..generate_pageview import Pagetags
+
+class TestNothing (unittest.TestCase):
+
+    def test_nothing (self):
+        pass
